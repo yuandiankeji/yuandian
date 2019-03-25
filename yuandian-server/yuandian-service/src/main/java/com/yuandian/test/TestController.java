47f90fbe98aef3516a700dd29b2ee0ff8c0775cd
luyufeng <luyufeng@5agame.cn>
2019-03-25 14:08:01 +0800

    Redisson模块

--- yuandian-server/yuandian-service/src/main/java/com/yuandian/test/TestController.java
+++ yuandian-server/yuandian-service/src/main/java/com/yuandian/test/TestController.java
@@ -1,5 +1,8 @@
 package com.yuandian.test;
 
+import com.yuandian.service.RedisService;
+import org.redisson.api.RList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/test")
 public class TestController {
+    @Autowired
+    private RedisService redisService;
+
     @GetMapping(value = "/success")
     public Object success() {
+        RList<String> list = redisService.getRList("testList");
+        list.add("a");
+        list.add("bb");
+        list.add("ccc");
         return "success";
     }
 }
