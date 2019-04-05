8a9d5555be000c4bbb389c9a527ae14b0d7a221e
luyufeng <yufenglu217393@sohu-inc.com>
2019-04-05 15:26:01 +0800

    状态码

--- yuandian-server/yuandian-service/src/main/java/com/yuandian/controller/LoginController.java
+++ yuandian-server/yuandian-service/src/main/java/com/yuandian/controller/LoginController.java
@@ -1,6 +1,12 @@
 package com.yuandian.controller;
 
+import com.yuandian.core.common.ResultModel;
+import com.yuandian.core.common.ResultStatus;
+import com.yuandian.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,7 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping(value = "/login")
+@RequestMapping(value = "/auth")
 public class LoginController {
 
+    @Autowired
+    private TokenService tokenService;
+
+    @RequestMapping(value = "/login", method = {RequestMethod.POST})
+    public ResultModel login(@RequestParam String username, @RequestParam String password) {
+        ResultModel resultModel = new ResultModel(ResultStatus.SUCCESS);
+        return resultModel;
+    }
+
+    @RequestMapping("/logout")
+    public ResultModel logout() {
+        ResultModel resultModel = new ResultModel(ResultStatus.SUCCESS);
+        return resultModel;
+    }
 }
