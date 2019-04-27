5a0621f3a880c863c5bf462d768e3ff17e414910
luyufeng <yufenglu217393@sohu-inc.com>
2019-04-27 18:37:30 +0800

    登录接口修改

--- yuandian-server/yuandian-controller/src/main/java/com/yuandian/login/LoginController.java
+++ yuandian-server/yuandian-controller/src/main/java/com/yuandian/login/LoginController.java
@@ -3,6 +3,7 @@ package com.yuandian.login;
 import com.yuandian.core.common.ResultModel;
 import com.yuandian.core.common.ResultStatus;
 import com.yuandian.domain.UserPO;
+import com.yuandian.entity.Token;
 import com.yuandian.service.TokenService;
 import com.yuandian.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,12 +25,13 @@ public class LoginController {
     private UserService userService;
 
     @RequestMapping(value = "/login", method = {RequestMethod.POST})
-    public ResultModel login(@RequestParam long uid, @RequestParam String password) {
+    public ResultModel login(@RequestParam long uid, @RequestParam String password, @RequestParam String deviceId) {
         UserPO userPO = userService.selectUserById(uid);
-        ResultModel resultModel = null;
+        ResultModel resultModel;
         if (userPO != null && userPO.getPassWord().equals(password)) {
              resultModel = new ResultModel(ResultStatus.SUCCESS);
-             resultModel.setContent(resultModel);
+            Token token = tokenService.createToken(uid, deviceId);
+            resultModel.setContent(token);
         } else {
             resultModel = ResultModel.error(ResultStatus.USERNAME_OR_PASSWORD_ERROR);
         }
@@ -39,6 +41,7 @@ public class LoginController {
 
     @RequestMapping("/logout/{uid}")
     public ResultModel logout(@PathVariable long uid) {
+        tokenService.deleteToken(uid);
         ResultModel resultModel = new ResultModel(ResultStatus.SUCCESS);
         return resultModel;
     }
