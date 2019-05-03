3319aeb5e9768b0b4a2641c377ce474ff1a04068
luyufeng <luyufengbj@163.com>
2019-05-03 19:42:02 +0800

    update

--- yuandian-server/yuandian-controller/src/main/java/com/yuandian/test/TestController.java
+++ yuandian-server/yuandian-controller/src/main/java/com/yuandian/test/TestController.java
@@ -2,17 +2,20 @@ package com.yuandian.test;
 
 import com.robert.vesta.service.intf.IdService;
 import com.yuandian.core.annotation.Authorization;
+import com.yuandian.core.common.ResultModel;
 import com.yuandian.entity.LoginPO;
+import com.yuandian.entity.UserPO;
 import com.yuandian.service.LoginService;
 import com.yuandian.core.utils.RedisService;
 import com.yuandian.service.TokenService;
+import com.yuandian.service.UserService;
 import org.redisson.api.RList;
 import org.redisson.api.RMap;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.HashMap;
 
 /**
  * @author: luyufeng
@@ -29,6 +32,9 @@ public class TestController {
     private IdService idService;
 
     @Autowired
+    private UserService userService;
+
+    @Autowired
     private TokenService tokenService;
 
     @Autowired
@@ -43,9 +49,17 @@ public class TestController {
         return "success";
     }
 
-    @GetMapping("/getId")
+    @PostMapping("/getId")
     public long getId() {
         return idService.genId();
+    }
+
+    @RequestMapping(value = "/clientTest", method = {RequestMethod.POST,RequestMethod.GET})
+    public ResultModel clientTest(@RequestParam String id) {
+        HashMap<String, String> res = new HashMap<>();
+        res.put("id", id);
+        return ResultModel.ok(res);
+
     }
 
     @Authorization
@@ -69,5 +83,28 @@ public class TestController {
         return loginService.selectByUid(uid);
     }
 
+    @GetMapping("/insertUser")
+    public String insertUser() {
+        UserPO userPO = getOneMockUser();
+        userService.insertUser(userPO);
+        return "success";
+    }
 
+    public UserPO getOneMockUser() {
+        UserPO userPO = new UserPO();
+        userPO.setUid(idService.genId());
+        userPO.setNickName("rick");
+        userPO.setAccount("178323wefw");
+        userPO.setBirthday(new Date());
+        userPO.setRegistration(new Date());
+        userPO.setHeadUrl("/user/1234566.jpg");
+        userPO.setMail("179979304@qq.com");
+        userPO.setPhoneNum("18910430596");
+        userPO.setQq("179979304");
+        userPO.setSex(1);
+        userPO.setSignature("在这里大家真诚交流");
+        userPO.setWechat("luyufeng_24");
+        userPO.sethAddress("我的地址");
+        return userPO;
+    }
 }
