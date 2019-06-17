76506f200541b597f20d4758ac7e17a289b76ee7
twjitm <twjitm@gail.com>
2019-06-17 23:25:16 +0800

    [fix]proto

--- yuandian-server/yuandian-netty/src/main/java/com/yuandian/server/core/net/MessageCmd.java
+++ yuandian-server/yuandian-netty/src/main/java/com/yuandian/server/core/net/MessageCmd.java
@@ -8,12 +8,44 @@ public final class MessageCmd {
      * 请求消息号
      */
     public static final int REQUEST_BASE = 1000;
+    /**
+     * 用户信息
+     */
     public static final int GET_USER_INFO = REQUEST_BASE + 1;
+    /**
+     * 登陆认证
+     */
     public static final int AUTH_ACCOUNT = REQUEST_BASE + 2;
+    /**
+     * todo 暂时不用（废弃）
+     */
     public static final int LOGIN = REQUEST_BASE + 3;
+    /**
+     * 发送消息
+     */
     public static final int SEND_CHAT = REQUEST_BASE + 4;
+    /**
+     * 获得聊天消息列表
+     */
     public static final int GET_CHAT_RECORD = REQUEST_BASE + 5;
+    /**
+     * 删除聊天消息
+     */
     public static final int DELETE_CHAT_RECORD = REQUEST_BASE + 6;
+    /**
+     * 阅读消息
+     */
+    public static final int READ_CAHT = REQUEST_BASE + 7;
+    /**
+     * 聊天好友列表
+     */
+    public static final int GET_CHAT_USER_LIST = REQUEST_BASE + 8;
+    /**
+     * 推出登陆
+     */
+    public static final int LOGOUT = REQUEST_BASE + 9;
+
+
 
 
     /**
