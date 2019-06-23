673a32b5dee66ba607b27153190d1d4b4cb90f03
luyufeng <luyufengbj@163.com>
2019-06-23 11:24:10 +0800

    fix

--- yuandian-server/yuandian-service/src/main/java/com/yuandian/mapper/FriendPoMapper.java
+++ yuandian-server/yuandian-service/src/main/java/com/yuandian/mapper/FriendPoMapper.java
@@ -16,7 +16,9 @@ import org.apache.ibatis.annotations.SelectProvider;
 import org.apache.ibatis.annotations.Update;
 import org.apache.ibatis.annotations.UpdateProvider;
 import org.apache.ibatis.type.JdbcType;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface FriendPoMapper {
     @SelectProvider(type=FriendPoSqlProvider.class, method="countByExample")
     int countByExample(FriendPoExample example);
