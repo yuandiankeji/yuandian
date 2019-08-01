d0b6ed303630653bf1ec917879dbc92362ce8de1
Wenjiang Tang <wenjiang.tang@kingsgroupgames.com>
2019-08-01 20:59:03 +0800

    [add]script

---
+++ yuandian-server/script/uploadJar.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+cd $0
+cd ../
+project_name='yuandian-netty/target/yuandian-netty-1.0-SNAPSHOT.jar'
+echo 'begin upload file'
+scp ${project_name}  yuandian:/root/
+echo 'upload file done'
+ssh yuandian "sh restart_yuandian_server.sh"
+echo 'server restart successful'
